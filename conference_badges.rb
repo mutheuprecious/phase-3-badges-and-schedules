d0d9a40e6d059721e2abcb90b495df60938c1f5a
Precious <preciousmaregwa1997@gmail.com>
2022-06-11 06:25:11 +0300

    complete assigment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator name_list
+    name_list.map {|name| badge_maker(name)}
+  end
+  
+  def assign_rooms name_list
+    name_list.collect.with_index(1) do |value,index|
+      "Hello, #{value}! You'll be assigned to room #{index}!"
+    end
+  end
+  
+  def printer name_list
+    batch_badge_creator(name_list).each do |name|
+      puts name
+    end
+    assign_rooms(name_list).each do |room| 
+      puts room
+    end
+  end
